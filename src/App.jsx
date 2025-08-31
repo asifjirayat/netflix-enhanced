a0428acd43167a07d366518620346365a4748fd2
Asif Jirayat <asifjirayat@gmail.com>
2025-08-31 12:55:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center text-3xl text-red-700">
+      Netflix Enhanced
+    </div>
+  );
+};
+
+export default App;
