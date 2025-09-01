de4c4855bb1fce7bfe74f00c11f65c7a8cf32e25
Asif <asifjirayat@gmail.com>
2025-09-01 16:28:57 +0530

    Modified: Removed intro component (Friction point)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import StreamflixIntro from "./components/intro/StreamflixIntro.jsx";
 import Homepage from "./components/pages/Homepage.jsx";
 
 const App = () => {
-  const [showIntro, setShowIntro] = useState(true);
-
-  const handleIntroComplete = () => setShowIntro(false);
-
   return (
     <>
-      {showIntro ? (
-        <StreamflixIntro onComplete={handleIntroComplete} />
-      ) : (
-        <Homepage />
-      )}
+      <Homepage />
     </>
   );
 };
